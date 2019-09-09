ddce2504f65e91f936b90c63b2db1d4918f37a73
eqtstv <38236287+eqtstv@users.noreply.github.com>
2019-09-09 17:11:48 +0200

    Update 6_Detect_panagram.py

--- Python/6_Detect_panagram.py
+++ Python/6_Detect_panagram.py
@@ -1,14 +1,8 @@
-import string
-from functools import reduce
-
 def is_pangram(s):
-    repls = (' ', ''), (',', ''), ('!', ''), ('.', '')
     alph = set('abcdefghijklmnopqrstuvwxyz')
-    nums = set('1234567890')
-    comp_set = set(reduce(lambda a, kv: a.replace(*kv), repls, s).lower())     
-    comp_set = comp_set - nums
-    
-    if comp_set == alph:
-        return True
-    else:
+    s_set = set(s.lower())
+
+    if alph - alph.intersection(s_set):
         return False
+    else:
+        return True
